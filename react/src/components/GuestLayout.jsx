489a32e5bbd3c4a77ae3f8da956795a976946638
Thando Mini <tzone85@gmail.com>
2023-03-20 14:19:03 +0200

    added two layouts components, for admin as well as guest users with some minor logging off configuration

---
+++ react/src/components/GuestLayout.jsx
@@ -0,0 +1,24 @@
+import {Outlet} from "react-router-dom";
+import {LockClosedIcon} from "@heroicons/react/20/solid/index.js";
+
+export default function GuestLayout() {
+    return (
+        <div>
+            <div className="flex min-h-full bg-slate-300 items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="w-full max-w-md space-y-8">
+                    <div>
+                        <img
+                            className="mx-auto h-12 w-auto"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                            alt="Your Company"
+                        />
+
+                    </div>
+
+                <Outlet/>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,5 +1,15 @@
 export default function Surveys() {
-    return (
-        <div>Surveys</div>
+    return (<>
+            <header className="bg-white shadow">
+                <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
+                </div>
+            </header>
+            <main>
+                <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                    Surveys Content
+                </div>
+            </main>
+        </>
     )
 }
